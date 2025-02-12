984ab05b3f4dacbc67f4fbc3e6f2daaaafd0d9a9
Shinsha <kingfedfe7.kkg@gmail.com>
2025-02-12 11:53:44 +0700

    12/2/2568.py

--- Extract code.py
+++ Extract code.py
@@ -1,13 +1,11 @@
 # Source Extraction
 import requests
 import pandas as pd
-
-# จากเว็บ https://tjhunter.github.io/climate-trace-handbook/initial_analysis.html#country-emissions
 import ctrace as ct #pip install climate-trace, #pip install huggingface_hub
-from ctrace.constants import * 
+from ctrace.constants import * # จากเว็บ https://tjhunter.github.io/climate-trace-handbook/initial_analysis.html#country-emissions
 
 #ฟังก์ชั่นการทำงานของการดึงข้อมูล ของเว็บ ClimateWatch
-def urlCW(soruce_id, gas_id,max_pages=200):
+def urlCW(source_id, gas_id,max_pages=200):
     base_URL = "https://www.climatewatchdata.org/api/v1/data/historical_emissions"
     all_data = []
     page = 1
@@ -15,7 +13,7 @@ def urlCW(soruce_id, gas_id,max_pages=200):
     # อ่านค่า sector_id และ id_gas จากไฟล์มีให้้ใช้ในเว็บ
     while True:
         params = {
-            "source_ids[]": soruce_id,
+            "source_ids[]": source_id,
             "gas_ids[]": gas_id,
             "page": page,
             "per_page": 10000,
@@ -40,7 +38,7 @@ def urlCW(soruce_id, gas_id,max_pages=200):
         else:
             break
     final_df = pd.json_normalize(all_data)
-    final_df.to_csv("ClimateWatchDATA.csv", index=False)
+    final_df.to_csv("ClimateWatchDATA.csv", index=False)   
 source_id = [214,215,216,217]
 gas_id = [474,475,476,477]        
 # urlCW(source_id, gas_id)
@@ -62,11 +60,12 @@ def urlOWD():
             "https://ourworldindata.org/grapher/nitrous-oxide-emissions-by-sector.csv?v=1&csvType=full&useColumnShortNames=true"
         ]
     }
-    # ใช้for ดึงข้อมูล
+    
     dataframes = {}
+    # ใช้for ดึงข้อมูล
     # กำหนดก๊าซชื่อก๊าซ แล้วให้ url เป็น urllist สำหรับเก็บเป็น item
     for gas, url_list in urls.items():
-        dataframes[gas]= [pd.read_csv(url, storage_options={'User-Agent': 'Our World In Data data fetch/1.0'}) for url in url_list]
+        dataframes[gas]= [pd.read_csv(urls, storage_options={'User-Agent': 'Our World In Data data fetch/1.0'}) for urls in url_list]
         
     for gas, df in dataframes.items():
         # วนลูปในรายการ
@@ -77,26 +76,40 @@ def urlOWD():
             df.to_csv(filename, index=False)
 # urlOWD()
 
-#https://huggingface.co/datasets/tjhunter/climate-trace เยอะมาก มีโค้ดดึงโหลดได้ แต่ไฟล์ เกือบ 10gb
-#from datasets import load_dataset
-#ds = load_dataset("tjhunter/climate-trace")
+
 #ฟังก์ชั่นดึงข้อมูลจากเว็บ Our world in Data พื้นที่ตามจุดที่มีการเปิดเผย
 def urlCT():
     gases = ["co2e_100yr","co2", "n2o", "ch4"]
     years = [2021, 2022, 2023, 2024]
-    dataframes = {}
     
     for gas in gases:
-        all_gas = []
         for year in years:
             url = f"https://api.c10e.org/v6/app/assets?year={year}&gas={gas}&subsectors="
             response = requests.get(url)
             json_data = response.json()
             df = pd.json_normalize(json_data["assets"])
-            print(f"Gas: {gas}")
+            print(f"Gas: {gas}, Year: {year}")
             print(df)
             
-    #ตัวที่สองที่เจอ
-    cedf = ct.read_country_emissions(GAS_LIST)
-    print(cedf)
-# urlCT()
+    # #ตัวที่สองที่เจอ
+    # cedf = ct.read_country_emissions(GAS_LIST)
+    # print(cedf)
+urlCT()
+
+#รับ้เอาค่าที่มากที่สุดแต่ละปีีมา
+def urlCT2():
+    gases = ["co2e_100yr","co2", "n2o", "ch4"]
+    years = [2021, 2022, 2023, 2024]
+    
+    for gas in gases:
+        for year in years:
+            url = f"https://api.c10e.org/v6/app/emissions?years={year}&gas={gas}&subsectors=&excludeForestry=true"
+            response = requests.get(url)
+            json_data = response.json()
+            
+            if "totals" in json_data:
+                total = json_data["totals"]
+                print(f"Gas: {gas}, Year: {year}, Total Value: {total['value']}")
+urlCT2()
+
+
