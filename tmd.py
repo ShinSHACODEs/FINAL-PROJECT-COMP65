9a30ded220c819aaa25b9db0ab8bf5be261254a2
Shinsha <kingfedfe7.kkg@gmail.com>
2025-05-04 18:15:54 +0700

    Update tmd.py

--- tmd.py
+++ tmd.py
@@ -10,7 +10,7 @@ file_id = "1OGcwxYAaBsL0CQlucI6nxc6HSoT3NtLB"
 url = f"https://drive.google.com/uc?id={file_id}"
 gdown.download(url, "TMDdata.csv", quiet=False)
 read = pd.read_csv("TMDdata.csv")
-print(read)
+print(read.head())
 
 def tmd():
     url = "https://www.tmd.go.th/climate/daily"
