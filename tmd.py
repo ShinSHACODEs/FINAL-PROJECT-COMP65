416328dcac74d483d12b96c4e3ddf525cc66c48b
Shinsha <kingfedfe7.kkg@gmail.com>
2025-05-04 21:48:21 +0700

    Update tmd.py

--- tmd.py
+++ tmd.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 from datetime import datetime, timedelta
+import gdown
 import os
 
 def tmd():
     url = "https://www.tmd.go.th/climate/daily"
     browser = start_firefox(url, headless=True)
     time.sleep(5)  # รอให้โหลดเสร็จ
-
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     browser.quit()
 
@@ -22,8 +22,7 @@ def tmd():
         for row in table.find_all('tr'):
             cells = row.find_all('td')
             row_data = [cell.get_text(strip=True) for cell in cells]
-            if row_data and row_data[0] != 'กำลังโหลดข้อมูล...' and len(row_data) == 8:
-                rows.append(row_data)
+            rows.append(row_data)
 
     columns = [
         "สถานีอุตุนิยมวิทยา", "อุณหภูมิสูงสุด", "อุณหภูมิต่ำสุด", "ทิศ",
@@ -32,36 +31,29 @@ def tmd():
 
     df_new = pd.DataFrame(rows, columns=columns)
     df_new["วันที่"] = yesterday
-
-    file_path = "TMDdata.csv"
-    if os.path.exists(file_path):
-        try:
-            df_existing = pd.read_csv(file_path, encoding="utf-8-sig")
-        except Exception as e:
-            print("เกิดข้อผิดพลาดในการอ่านไฟล์:", e)
-            df_existing = pd.DataFrame()
-
-        # เช็คว่ามีข้อมูลของวันนั้นแล้วหรือยัง
-        is_duplicate = df_existing.merge(
-            df_new[["สถานีอุตุนิยมวิทยา", "วันที่"]],
-            on=["สถานีอุตุนิยมวิทยา", "วันที่"],
-            how="inner"
-        )
-
+    print(df_new)
+    
+    file_id = "1e421mydwndQDaCaNDCkEt6468HBLS72H"
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, "TMDdata.csv", quiet=False)
+    df_old = "TMDdata.csv"
+    
+    if os.path.exists(df_old):
+        df_old = pd.read_csv(url, encoding="utf-8-sig")
+        
+        is_duplicate = df_old.merge(
+                    df_new[["สถานีอุตุนิยมวิทยา", "วันที่"]],
+                    on=["สถานีอุตุนิยมวิทยา", "วันที่"],
+                    how="inner"
+                )
+        
         if not is_duplicate.empty:
             print("ข้อมูลของเมื่อวานมีอยู่แล้ว ไม่ต้องบันทึก")
             return
-
-        # รวมและลบซ้ำ
-        df_combined = pd.concat([df_existing, df_new], ignore_index=True).drop_duplicates(
-            subset=["สถานีอุตุนิยมวิทยา", "วันที่"], keep="last"
-        )
-    else:
-        df_combined = df_new
-
-    # บันทึก
-    df_combined.to_csv(file_path, index=False, encoding="utf-8-sig")
-    print("บันทึกข้อมูลสำเร็จ:", file_path)
-
-if __name__ == "__main__":
-    tmd()
+    else:    
+        df_combined = pd.concat([df_old, df_new], ignore_index=True).drop_duplicates(
+        subset=["สถานีอุตุนิยมวิทยา", "วันที่"], keep="last")
+        
+    df_combined.to_csv(index=False, encoding="utf-8-sig")
+    print("บันทึกข้อมูลสำเร็จ")
+tmd()
