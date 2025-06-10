58327e8b014d792780b2ec728cc9a069f1612b26
Shinsha <kingfedfe7.kkg@gmail.com>
2025-06-10 14:32:48 +0700

    Update OurWorldData.py

--- OurWorldData.py
+++ OurWorldData.py
@@ -15,7 +15,7 @@ def urlOWD():
     # URLs สำหรับแต่ละก๊าซ
     urls = {
         "CO2": [
-            "https://ourworldindata.org/grapher/cumulative-co-emissions.csv?v=1&csvType=full&useColumnShortNames=true",
+            "https://ourworldindata.org/grapher/annual-co-emissions-by-region.csv?v=1&csvType=full&useColumnShortNames=true",
             "https://ourworldindata.org/grapher/co-emissions-by-sector.csv?v=1&csvType=full&useColumnShortNames=true",
         ],
         "CH4": [
