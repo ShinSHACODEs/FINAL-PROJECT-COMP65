f6e2fd63caad78aa2f0f26ed638da0a6bf58218d
Shinsha <kingfedfe7.kkg@gmail.com>
2025-09-13 17:39:42 +0700

    Update CarbonMoni.py

--- CarbonMoni.py
+++ CarbonMoni.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 import gdown
 
-file_id = "1XWpKhuINayLYAT_f_A-yA18LxuLVN2MW"
+file_id = "161FmRz3oR8kGsSwVnHFLyO5JmCDlvXl6"
 url = f"https://drive.google.com/uc?id={file_id}"
 gdown.download(url, "carbon_monitor.csv", quiet=False)
 df = pd.read_csv("carbon_monitor.csv")
@@ -53,3 +53,4 @@ def download_carbon_monitor_csv():
     print(f"ลบไฟล์ดาวน์โหลดแล้ว: {filename}")
 
 download_carbon_monitor_csv()
+
